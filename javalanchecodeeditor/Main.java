be78282394323ac8ff5e82c4d52c0d9f4c27873b
paolagaribay <54656030+paolagaribay@users.noreply.github.com>
2019-12-05 01:18:52 -0600

    Print class loader

--- javalanchecodeeditor/Main.java
+++ javalanchecodeeditor/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         new JavalancheCodeEditor();
+        PrintClassLoader();
+    }
+    public static void PrintClassLoader() {
+        System.out.println("Classloader of Main: "
+                + Main.class.getClassLoader());
+
+        System.out.println("Classloader of JavalancheCodeEditor: "
+                + JavalancheCodeEditor.class.getClassLoader());
+
+        System.out.println("Classloader of KeywordCounter: "
+                + KeywordCounter.class.getClassLoader());
     }
 }
